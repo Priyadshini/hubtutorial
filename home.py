3dd9037e4e9f6ac98f13b9ede357395dce250213
Priyadshini <88380282+Priyadshini@users.noreply.github.com>
2021-09-14 12:59:52 +0530

    return statement changed

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello !!!"
+    return "Hello !!! \n Welcome to home page"
 
 if __name__ == '__main__':
     app.run(debug = True)
