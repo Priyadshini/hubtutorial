5caad31ce7bcd8043641fe030b3caa2c784f8196
Priyadharshini <aspriya1998@gmail.com>
2021-09-17 11:10:54 +0530

    added new route

--- home.py
+++ home.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def home_page():
     return "Hello !!! \n Welcome to home pagngae"
 
+
+@app.route('/login')
+def login():
+    return "<h1>this is login page </h1>"
+
+
 if __name__ == '__main__':
     app.run(debug = True)
     
